c23241eb95c377c60fdd0d672d9a3939a6d28ef6
Lobo <lobo@davincicoats.com>
2023-06-12 19:57:23 -0400

    3rd correct

--- store/index.js
+++ store/index.js
@@ -1,22 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import cart from "./cartSlice";
-const reducers= combineReducers({cart});
+import expandSidebar from "./ExpandSlice";
+import dialog from "./DialogSlice";
+const reducers = combineReducers({ cart, expandSidebar, dialog });
 
-const config= {
-    key: "root",
-    storage : AsyncStorage,
+const config = {
+  key: "root",
+  storage,
 };
 
-const reducer = persistReducer(config,reducers);
+const reducer = persistReducer(config, reducers);
 
-const store=configureStore({
-    reducer:reducer,
-    devTools:process.env.NODE_ENV !== "production",
-    middleware: [thunk],
+const store = configureStore({
+  reducer: reducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
 });
 
 export default store;
