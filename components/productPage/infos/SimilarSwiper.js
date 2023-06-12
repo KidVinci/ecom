630eb1a487c59e81ce7f94dd9083128aac99d985
Lobo <lobo@davincicoats.com>
2023-06-12 15:59:01 -0400

    3rd correct

--- components/productPage/infos/SimilarSwiper.js
+++ components/productPage/infos/SimilarSwiper.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { simillar_products } from "../../../data/products";
+import { simillar_products } from "@/data/product";
 import styles from "./styles.module.scss";
 import { useRef, useState } from "react";
 // Import Swiper React components
