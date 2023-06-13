63759b0551e497082521f293d839e0c58e1d91a8
Lobo <lobo@davincicoats.com>
2023-06-13 01:19:38 -0400

    5th should be right

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,6 +1,6 @@
 import styles from "./styles.module.scss";
 import MainSwiper from "./swiper";
-import Offers from "./offers";
+
 import { useSession } from "next-auth/react";
 import Menu from "./Menu";
 import Link from "next/link";
